ee475cdc07aa5ec4231c5147dd8d33c94f3c2fee
Sam Bowden-Williams <SBowdenWilliams@spartaglobal.com>
2019-01-11 11:43:57 +0000

    first commit

---
+++ JS/main.js
@@ -0,0 +1,15 @@
+//alert ("Hello!");
+console.log("Hi in console");
+var number = 90;
+console.log(number);
+
+var num1 = 10;
+var num2 = [5, 3, 4, true];
+
+sum = num1 + num2;
+
+
+console.log(num1, "plus",num2[1], "equals", sum);
+
+
+
